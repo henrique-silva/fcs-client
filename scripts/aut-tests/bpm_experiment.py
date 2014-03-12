15f7ce1d99820b2fab3ac753a966ba5125cdebc7
Daniel Tavares <daniel.tavares@lnls.br>
2014-03-12 13:00:03 -0300

    bpm_experiment.py: differentiate when BPM amplitude signals are centered in IF (e.g. ADC datapath) or baseband (e.g. turn-by-turn, FOFB) correct fixed value of FOFB decimation rate to 1112 change parameter name from data_rate_decim_factor to data_rate_decimation_ratio do not set BPM Kx and Ky anymore include sleep after sending RFFE commands to prevent RFFE refusing successive connection requests with small time interval automatically fill data_file_structure and data_file_format parameters of metadata

--- scripts/aut-tests/bpm_experiment.py
+++ scripts/aut-tests/bpm_experiment.py
@@ -3,6 +3,7 @@ from os.path import basename
 from time import time
 import hashlib
 from time import strftime, gmtime
+from time import sleep
 from math import floor
 import subprocess
 
@@ -31,25 +32,28 @@ class BPMExperiment():
 
     def run(self, data_filename, datapath):
         if datapath == 'adc':
-            data_rate_decim_factor = '1'
+            data_rate_decimation_ratio = '1'
             acq_channel = '0'
             acq_npts = '100000'
+            data_file_structure = 'bpm_amplitudes_if'
         elif datapath == 'tbt':
-            data_rate_decim_factor = self.metadata['adc_clock_sampling_harmonic'].split()[0] # FIXME: data_rate_decim_factor should be ideally read from FPGA
+            data_rate_decimation_ratio = self.metadata['adc_clock_sampling_harmonic'].split()[0] # FIXME: data_rate_decim_factor should be ideally read from FPGA
             acq_channel = '1'
             acq_npts = '100000'
+            data_file_structure = 'bpm_amplitudes_baseband'
         elif datapath == 'fofb':
-            data_rate_decim_factor = '1000' # FIXME: data_rate_decim_factor should be ideally read from FPGA
+            data_rate_decimation_ratio = '1112' # FIXME: data_rate_decim_factor should be ideally read from FPGA
             acq_channel = '3'
             acq_npts = '1000000'
+            data_file_structure = 'bpm_amplitudes_baseband'
 
         deswitching_phase_offset = str(int(self.metadata['dsp_deswitching_phase'].split()[0]) - int(self.metadata['rffe_switching_phase'].split()[0]))
 
         # Run FPGA configuration commands
         command_argument_list = ['fcs_client']
         command_argument_list.extend(['--setdivclk', self.metadata['rffe_switching_frequency_ratio'].split()[0]])
-        command_argument_list.extend(['--setkx', self.metadata['bpm_Kx'].split()[0]])
-        command_argument_list.extend(['--setky', self.metadata['bpm_Ky'].split()[0]])
+        #command_argument_list.extend(['--setkx', self.metadata['bpm_Kx'].split()[0]])
+        #command_argument_list.extend(['--setky', self.metadata['bpm_Ky'].split()[0]])
         command_argument_list.extend(['--setphaseclk', deswitching_phase_offset])
         command_argument_list.extend(['--setsw' + self.metadata['rffe_switching'].split()[0]])
         command_argument_list.extend(['--setwdw' + self.metadata['dsp_sausaging'].split()[0]])
@@ -73,6 +77,7 @@ class BPMExperiment():
         command_argument_list.extend(['--setrffehostname', self.rffe_hostname])
         if not self.debug:
             subprocess.call(command_argument_list)
+            sleep(0.2) # FIXME: it seems RFFE controller (mbed) doesn't realize the connection has been closed
         else:
             print(command_argument_list)
 
@@ -142,8 +147,10 @@ class BPMExperiment():
         config_automatic_lines = [];
         config_automatic_lines.append('data_original_filename = ' + data_filename_basename + '\n')
         config_automatic_lines.append('data_signature = ' + filesignature + '\n')
-        config_automatic_lines.append('dsp_data_rate_decim_factor = ' + data_rate_decim_factor + '\n')
+        config_automatic_lines.append('dsp_data_rate_decimation_ratio = ' + data_rate_decimation_ratio + '\n')
         config_automatic_lines.append('timestamp_start = ' + timestamp_start + '\n')
+        config_automatic_lines.append('data_file_structure = ' + data_file_structure + '\n')
+        config_automatic_lines.append('data_file_format = ascii\n')
         #config_automatic_lines.append('adc_board_temperature = ' + '0' + ' C\n') #TODO: implement ADC temperature read on FPGA
         #config_automatic_lines.append('rffe_board_temperature = ' + '0' + ' C\n') #TODO: implement RFFE temperature read on FPGA
 
