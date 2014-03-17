7550ca9c3d8da6bc64f5dbc557b14c0608a64225
Daniel Tavares <daniel.tavares@lnls.br>
2014-03-17 10:12:04 -0300

    run_sweep.py: add DSP sausaging sweep

--- scripts/aut-tests/run_sweep.py
+++ scripts/aut-tests/run_sweep.py
@@ -13,6 +13,7 @@ data_file_path = sys.argv[2]
 exp = BPMExperiment('localhost', '')
 
 rffe_switching_sweep = ['off', 'on']
+dsp_sausaging_sweep = ['off', 'on']
 
 datapaths = ['adc', 'tbt', 'fofb']
 
@@ -49,53 +50,59 @@ while True:
         # Sweep RFFE channels switching (switching on or off)
         for rffe_switching in rffe_switching_sweep:
             exp.metadata['rffe_switching'] = rffe_switching
-            exp.metadata['dsp_sausaging'] = rffe_switching
 
-            # Sweep all RFFE attenuator values which respect power level thresholds
-            for att_value_set in att_combinations:
-                # Check if power level at each RFFE stage is lower than the allowed threshold
-                power_level = float(exp.metadata['rffe_signal_carrier_maxpower'].split()[0])
+            # Sweep sausaging (sausaging on or off) when RFFE channels switching is on
+            for dsp_sausaging in dsp_sausaging_sweep:
+                if rffe_switching == 'off' and dsp_sausaging == 'on':
+                    break
+                else:
+                    exp.metadata['dsp_sausaging'] = dsp_sausaging
 
-                exceeded_threshold = False
-                i = 0
-                for att_value in att_value_set:
-                    power_level = power_level + rffe_gains[i] - float(att_value)
-                    if power_level > rffe_power_thresholds[i]:
-                        exceeded_threshold = True
-                        break
-                    i = i+1
+                    # Sweep all RFFE attenuator values which respect power level thresholds
+                    for att_value_set in att_combinations:
+                        # Check if power level at each RFFE stage is lower than the allowed threshold
+                        power_level = float(exp.metadata['rffe_signal_carrier_maxpower'].split()[0])
 
-                if exceeded_threshold:
-                    continue
+                        exceeded_threshold = False
+                        i = 0
+                        for att_value in att_value_set:
+                            power_level = power_level + rffe_gains[i] - float(att_value)
+                            if power_level > rffe_power_thresholds[i]:
+                                exceeded_threshold = True
+                                break
+                            i = i+1
 
-                # Write attenuation values to metadata
-                exp.metadata['rffe_attenuators'] = '';
-                for att_value in att_value_set:
-                    if exp.metadata['rffe_attenuators']:
-                        exp.metadata['rffe_attenuators'] = exp.metadata['rffe_attenuators'] + ', '
-                    exp.metadata['rffe_attenuators'] = exp.metadata['rffe_attenuators'] + str(att_value) + ' dB'
+                        if exceeded_threshold:
+                            continue
 
-                # Assure that no file or folder will be overwritten
-                ntries = 1;
-                while True:
-                    data_filenames = []
-                    for datapath in datapaths:
-                        data_filenames.append(os.path.join(os.path.normpath(data_file_path), 'switching_' + exp.metadata['rffe_switching'], datapath, 'data_' + str(ntries) + '_' + datapath + '.txt'))
+                        # Write attenuation values to metadata
+                        exp.metadata['rffe_attenuators'] = '';
+                        for att_value in att_value_set:
+                            if exp.metadata['rffe_attenuators']:
+                                exp.metadata['rffe_attenuators'] = exp.metadata['rffe_attenuators'] + ', '
+                            exp.metadata['rffe_attenuators'] = exp.metadata['rffe_attenuators'] + str(att_value) + ' dB'
 
-                    ntries = ntries+1
-                    if all(not os.path.exists(data_filename) for data_filename in data_filenames):
-                        break
+                        # Assure that no file or folder will be overwritten
+                        ntries = 1;
+                        while True:
+                            data_filenames = []
+                            for datapath in datapaths:
+                                data_filenames.append(os.path.join(os.path.normpath(data_file_path), 'switching_' + exp.metadata['rffe_switching'] + '_sausaging_' + exp.metadata['dsp_sausaging'], datapath, 'data_' + str(ntries) + '_' + datapath + '.txt'))
 
-                print(str.rjust('Run #' + str(nexp), 12) + ': RFFE switching ' + exp.metadata['rffe_switching'] + '; RFFE attenuators: ' + exp.metadata['rffe_attenuators'] + ' ')
-                nexp = nexp+1
+                            ntries = ntries+1
+                            if all(not os.path.exists(data_filename) for data_filename in data_filenames):
+                                break
 
-                for i in range(0,len(data_filenames)):
-                    print('        Running ' + datapaths[i] + ' datapath...', end='')
-                    sys.stdout.flush()
-                    exp.run(data_filenames[i], datapaths[i])
-                    print(' done. Results in: ' + data_filenames[i])
+                        print(str.rjust('Run #' + str(nexp), 12) + ': RFFE switching ' + exp.metadata['rffe_switching'] + '; RFFE attenuators: ' + exp.metadata['rffe_attenuators'] + '; DSP sausaging ' + exp.metadata['dsp_sausaging'] + ' ')
+                        nexp = nexp+1
 
-                print('')
+                        for i in range(0,len(data_filenames)):
+                            print('        Running ' + datapaths[i] + ' datapath...', end='')
+                            sys.stdout.flush()
+                            exp.run(data_filenames[i], datapaths[i])
+                            print(' done. Results in: ' + data_filenames[i])
+
+                        print('')
 
         print('The experiment has run successfully!\n');
         input_text = input('Press ENTER to load a new experiment setting from \'' + os.path.abspath(input_metadata_filename) + '\'.\nType \'q\' and press ENTER to quit.\n')
