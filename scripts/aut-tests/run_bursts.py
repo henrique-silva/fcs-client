7dc1493b79e543bf0a5f8348b6c2a9322830cb00
Daniel Tavares <daniel.tavares@lnls.br>
2014-03-19 17:10:09 -0300

    run_bursts.py: add number of minutes between two burst as input argument

--- scripts/aut-tests/run_bursts.py
+++ scripts/aut-tests/run_bursts.py
@@ -5,22 +5,29 @@ import time
 from time import sleep, strftime
 
 from run_single import run_single
+from run_sweep import run_sweep
 
 input_metadata_file_path = sys.argv[1]
 data_file_path = sys.argv[2]
 
+if len(sys.argv) > 3:
+    nminutes = float(sys.argv[3])
+else:
+    nminutes = 1
+
 last_experiment_time = time.time()
 
 while True:
-    print('\n\n\n=======================================================')
-    print('New experiment burst. Initiated at ' + strftime('%Y-%m-%dT%H:%M:%S') + '.')
-    print('=======================================================')
+    print('\n\n\n======================================================')
+    print('New experiment burst. Initiated at ' + strftime('%Y-%m-%d %H:%M:%S'))
+    print('======================================================')
     run_single([input_metadata_file_path, data_file_path, 'localhost', '0', False])
+    #run_sweep([input_metadata_file_path, data_file_path, 'localhost', '0', False])
 
     try:
         print('Waiting for next experiment burst... (press ctrl-c to stop)')
 
-        while time.time() - last_experiment_time < 60:
+        while time.time() - last_experiment_time < nminutes*60:
             sleep(1)
 
         last_experiment_time = time.time()
