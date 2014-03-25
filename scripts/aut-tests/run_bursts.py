b727be165ecd55c2d4fde39a4e806a71f8bf0482
Daniel Tavares <daniel.tavares@lnls.br>
2014-03-25 19:44:57 -0300

    run_bursts.py: add choice for the user to run single or sweep experiments

--- scripts/aut-tests/run_bursts.py
+++ scripts/aut-tests/run_bursts.py
@@ -15,14 +15,24 @@ if len(sys.argv) > 3:
 else:
     nminutes = 1
 
+if len(sys.argv) > 4:
+    run_type = sys.argv[4]
+else:
+    run_type = 'run_single'
+
 last_experiment_time = time.time()
 
 while True:
     print('\n\n\n======================================================')
     print('New experiment burst. Initiated at ' + strftime('%Y-%m-%d %H:%M:%S'))
     print('======================================================')
-    run_single([input_metadata_file_path, data_file_path, 'localhost', '0', False])
-    #run_sweep([input_metadata_file_path, data_file_path, 'localhost', '0', False])
+
+    if run_type == 'run_single':
+        run_single([input_metadata_file_path, data_file_path, 'localhost', '0', False])
+    elif run_type == 'run_sweep':
+        run_sweep([input_metadata_file_path, data_file_path, 'localhost', '0', False])
+    else:
+        break
 
     try:
         print('Waiting for next experiment burst... (press ctrl-c to stop)')
